eb138fb96d19ee1846e58a244a22bc2b76863c0a
HZ LIN <876407883@qq.com>
2018-12-18 00:15:11 +0800

    12.17 first time commit

---
+++ Assets/Scripts/LoadVoice.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadVoice : MonoBehaviour {
+
+    string basePath = "Voices/";
+    string voicePath = "106_1";
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+    public void play(){
+		string path = basePath + voicePath;
+        Debug.Log(path);
+		AudioClip audioClip = (AudioClip)Resources.Load(path, typeof(AudioClip));
+        if(audioClip == null){
+            Debug.Log("null");
+        }
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+		audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(0, 0, 100, 50), "play"))
+		{
+            play();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ServantVoice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServantVoice{
+
+    private int servantId;
+    private string resourcePath;
+    private string voiceWord;
+
+    public ServantVoice(int servantId, string resourcePath, string voiceWord){
+        this.servantId = servantId;
+        this.resourcePath = resourcePath;
+        this.voiceWord = voiceWord;
+    }
+
+    public string getVoicePath(){
+        return string.Format("{0}_{1}", servantId, resourcePath);
+    }
+
+}
